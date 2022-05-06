bf6400c27da45b483eb7e5f52ca978d68afa99de
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-05-06 15:28:36 -0700

    Remove redundant GameRadialMenu logic.

--- src/gui/game_radial_menu.gd
+++ src/gui/game_radial_menu.gd
@@ -3,21 +3,3 @@ extends RadialMenu
 
 
 # FIXME: LEFT OFF HERE: ---------------------
-
-
-func _on_level_touch_up(
-        pointer_screen_position: Vector2,
-        pointer_level_position: Vector2,
-        has_corresponding_touch_down: bool) -> void:
-    ._on_level_touch_up(
-            pointer_screen_position,
-            pointer_level_position,
-            has_corresponding_touch_down)
-    metadata.set_is_selected(false)
-
-
-func _on_center_area_touch_up(
-        touch_position: Vector2,
-        is_already_handled: bool) -> void:
-    ._on_center_area_touch_up(touch_position, is_already_handled)
-    metadata.set_is_player_control_active(true)
